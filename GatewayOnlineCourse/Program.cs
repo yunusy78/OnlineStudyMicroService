8775539874d0cba4849b37e36e0fcc20692c8846
Yunus Yigit <yunusy@uia.no>
2023-10-07 21:04:07 +0200

    fix:ocelot

--- GatewayOnlineCourse/Program.cs
+++ GatewayOnlineCourse/Program.cs
@@ -4,13 +4,13 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
 
-//builder.Services.AddHttpClient<TokenExhangeDelegateHandler>();
-/*builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
 {
     options.Authority = builder.Configuration["IdentityServerUrl"];
     options.Audience = "resource_gateway";
     options.RequireHttpsMetadata = false;
-});*/
+});
 
 builder.Services.AddOcelot();
 var app = builder.Build();
